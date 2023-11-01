090fe78a6b909a6c964f205db9fd95f2dade007a
MaryModest'/0/ <20800073@usc.edu.ph>
2023-11-01 16:37:10 +0800

    Added in Parent Pointer Representation Codes
    The ParentPointerRepresentation.c file is the original implementation which is used generally. Meanwhile the PPRAssignment.c file is the code for ma'am's activity wherein the nodes are to be in sequential order.

---
+++ TreesBasics/PPRAssignment.c
@@ -0,0 +1,108 @@
+/* * * * * * * * * * * * * * * * * * 
+* AUTHOR: MODESTO, MARY CHEVEL P.   *
+* SCHOOL YEAR: 2023 - 2024          *
+* DSA PROF: MA'AM PINEAPPLE         *
+* DATE: NOVEMBER 1, 2023            *
+* * * * * * * * * * * * * * * * * */
+
+/*NOTE: THE CONTRAINTS FOR THIS IMPLEMENTATION ARE THAT THE NODES HAVE TO BE IN SEQUENTIAL ORDER*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+#define ROOT -1
+#define DNS -2
+
+typedef int TREE[MAX];
+typedef int node;
+typedef int label;
+
+void Display(TREE T);
+node Parent(node N, TREE T);
+node Leftmost_Child(node N, TREE T);
+node Right_Sibling(node N, TREE T);
+label Label(node N, TREE T);
+node Root(TREE T);
+void Initialize(TREE T, int values[]);
+void MakeNULL(TREE T);
+
+int main()
+{
+    TREE myTree;
+    int myArr[] = {ROOT, 0, 0, 0, 1, 2, 2, 6, 6, 2};
+
+    //INITIALIZE MY TREE
+    Initialize(myTree, myArr);
+    Display(myTree);
+
+    //PRINT OUT THE LEFTMOST CHILD OF EVERY NODE
+    int x;
+    for (x = 0; x < MAX; ++x) {
+        printf("[%d] => %d\n", x, Leftmost_Child(x, myTree));
+    }
+    printf("\n");
+
+    //PRINT OUT THE RIGHT SIBLING OF EVERY NODE
+    for (x = 0; x < MAX; ++x) {
+        printf("[%d] => %d\n", x, Right_Sibling(x, myTree));
+    }
+    printf("\n");
+    return 0;
+}
+
+void Display(TREE T) {
+    int x;
+    for (x = 0; x < MAX; ++x) {
+        printf("[%d] => ", x);
+        if (T[x] == ROOT) {
+            printf("ROOT");
+        } else if (T[x] == DNS) {
+            printf("DNS");
+        } else{
+            printf("%d", T[x]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+node Parent(node N, TREE T) {
+    return T[N] != DNS && T[N] != ROOT ? T[N] : DNS;
+}
+
+node Leftmost_Child(node N, TREE T) {
+    int retVal = DNS;
+    
+    if (T[N] != DNS && T[N] != MAX - 1) {
+        int x;
+        for (x = N; x < MAX && T[x] != N; ++x) {}
+        retVal = (x == MAX ? DNS : x);
+    }
+    return retVal;
+}
+
+node Right_Sibling(node N, TREE T) {
+    return T[N] == ROOT || T[N] == DNS || N == MAX - 1 ? DNS : T[N] == T[N + 1] ? N + 1 : DNS;
+}
+
+label Label(node N, TREE T) {
+    return T[N];
+}
+
+node Root(TREE T) {
+    return 0;
+}
+
+void Initialize(TREE T, int values[]) {
+    int x;
+    for (x = 0; x < MAX; ++x) {
+        T[x] = values[x];
+    }
+}
+
+void MakeNULL(TREE T) {
+    int x;
+    for (x = 0; x < MAX; ++x) {
+        T[x] = DNS;
+    }
+}
